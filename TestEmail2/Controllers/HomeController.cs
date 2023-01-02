50e96e690ad89201d08495f9dcc7071b9a3812c4
phongtd16061994 <phongtd@devblock.net>
2023-01-02 14:18:58 +0700

    Add project files.

---
+++ TestEmail2/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TestEmail2.Models;
+
+namespace TestEmail2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpPost]
+        public IActionResult Index(string to, string subject, string body)
+        {
+            try
+            {
+                Thread email = new Thread(delegate ()
+                {
+                    Sendmail.Email(to, subject, body);
+                });
+                email.IsBackground = true;
+                email.Start();
+                TempData["alert"] = "Send Success";
+            }
+            catch(Exception)
+            {
+                TempData["alert"] = "Problem Send";
+            }
+            return Redirect("Home/Index");
+        }
+    }
+}
---
+++ TestEmail2/Sendmail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace TestEmail2
+{
+    public class Sendmail
+    {
+        public static void Email(string to, string subject, string body)
+        {
+            MailMessage mm = new();
+            mm.To.Add(new MailAddress(to));
+            mm.From = new MailAddress("tech-admin@devblock.net");
+            mm.Subject = subject;
+            mm.IsBodyHtml = true;
+            mm.Priority = MailPriority.High;
+            SmtpClient smtpClient = new();
+            smtpClient.Host = "smtp.office365.com";
+            smtpClient.Port = 587;
+            smtpClient.EnableSsl = true;
+            smtpClient.Credentials = new NetworkCredential("tech-admin@devblock.net", "MadLife123@");
+            smtpClient.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+            smtpClient.Send(mm);
+        }
+    }
+}
